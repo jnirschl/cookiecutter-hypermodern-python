8ba21b08592d161cca9e25201b19e2d156df507b
Jeff Nirschl <jnirschl@stanford.edu>
2022-09-27 17:32:21 -0700

    Update skeleton __main__.py with tests
    poetry add python-dotenv

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,12 +1,38 @@
-"""Command-line interface."""
+#!/usr/bin/env python3
+"""Command-line interface for {{cookiecutter.package_name}}."""
+import logging
+from pathlib import Path
+
 import click
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
 
 @click.command()
+@click.argument(
+    "input-dir", type=click.Path(exists=True, path_type=Path),
+)
+@click.option(
+    "--dry-run", is_flag=True, help="Perform a trial run with no changes made."
+)
 @click.version_option()
-def main() -> None:
-    """{{ cookiecutter.project_name.replace('-', ' ').title() }}."""
+def main(input_dir: Path, dry_run: bool = False,) -> None:
+    """Docstring."""
+    logger = logging.getLogger(__name__)
+    logger.info(f"Input directory: {input_dir}")
+    if dry_run:
+        logger.info(" Dry run ")
 
 
 if __name__ == "__main__":
-    main(prog_name="{{cookiecutter.project_name}}")  # pragma: no cover
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+
+    main()
---
+++ {{cookiecutter.project_name}}/tests/test_main.py
@@ -0,0 +1,32 @@
+"""Test cases for the __main__ module."""
+import pytest
+from click.testing import CliRunner
+
+from {{cookiecutter.package_name}} import __main__
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces."""
+    return CliRunner()
+
+
+class TestMain:
+    """Test cases for the main() function."""
+
+    def test_main_dry_run(self, runner: CliRunner) -> None:
+        """Test dry run."""
+        result = runner.invoke(__main__.main, ["--dry-run"])
+        assert result.exit_code == 0
+
+    def test_main_success(runner: CliRunner) -> None:
+        """Test successful run."""
+        result = runner.invoke(__main__.main)
+        assert result.exit_code == 0
+
+
+    def test_main_version(runner: CliRunner) -> None:
+        """Test version."""
+        result = runner.invoke(__main__.main, ["--version"])
+        assert result.exit_code == 0
+        assert result.output == f"{__main__.__version__}
