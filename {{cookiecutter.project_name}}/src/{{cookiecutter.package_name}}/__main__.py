77e8e5571d84b3d27f8d643cf3e9c1c9f4c0c4a8
Jeffrey Nirschl <jnirschl@stanford.edu>
2024-02-26 22:01:13 -0800

    refactor: updated version of __main__.py

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python3
 """Command-line interface for {{cookiecutter.package_name}}."""
-import logging
 from pathlib import Path
+from typing import Optional
 
 import click
 from dotenv import find_dotenv
 from dotenv import load_dotenv
+from loguru import logger
 
 from {{cookiecutter.package_name}} import __version__
 
 @click.command()
-@click.argument("input-dir", type=click.Path(exists=True, path_type=Path))
+@click.argument(
+    "input-file", type=click.Path(dir_okay=False, exists=True, path_type=Path)
+)
+@click.option(
+    "--output-dir", type=click.Path(file_okay=False, exists=False, path_type=Path)
+)
 @click.option("--dry-run", is_flag=True, help="Perform a trial run with no changes.")
-@click.version_option(__version__)
+@click.version_option()
 def main(
-    input_dir: Path,
-    dry_run: bool = False,
+        input_file: Path,
+        output_dir: Optional[Path] = None,
+        dry_run: bool = False,
 ) -> None:
     """Docstring."""
-    logger = logging.getLogger(__name__)
-    logger.info(f"Input directory: {input_dir}")
+    output_dir = input_file.parent if output_dir is None else output_dir.resolve()
+
+    project_dir = Path(__file__).resolve().parents[2]
+    log_dir = project_dir.joinpath("logs")
+    logger.add(
+        log_dir.joinpath(f"{Path(__file__).stem}.log"),
+        rotation="1 week",
+        level="INFO",
+    )
+
+    logger.info(f"Input file: {input_file}")
+    logger.info(f"Output directory: {output_dir}")
+
     if dry_run:
-        logger.info("Dry run")
-    else:
-        logger.info("Success!")
+        logger.info("Dry run: no changes will be made.")
+        return
 
 
 if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
     main()
